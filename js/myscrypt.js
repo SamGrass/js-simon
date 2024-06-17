9a4e5a61a168ff3575086625e07678325c6df6d0
Samgrass <samuelegrassetti01@gmail.com>
2024-06-17 16:42:31 +0200

    aggiungo oggetti e commenti js

---
+++ js/myscrypt.js
@@ -0,0 +1,8 @@
+// creo gioco in cui compaiono 5 numeri diversi, dopo 30 secondi scompaiono e l'utente digita i numeri che ha visto, dopo dico quanti e quali sono giusti
+// compaiono 5 numeri diversi
+
+// dopo 30 secondi scompaiono
+
+// l'utente digita i numeri che ha visto
+
+// dico quanti e quali sono giusti
